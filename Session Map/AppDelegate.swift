b6238c8573a214c63607185d840068116ac3d804
grandviewsound <kevincpeters@me.com>
2021-02-11 22:52:56 -0500

    Initial Commit

---
+++ Session Map/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Session Map
+//
+//  Created by Kevin Peters on 2/11/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
